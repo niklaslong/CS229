5ffd7371db899fd82e5c2177fa7d5fc7356a3abc
Niklas Long <niklas@long.ch>
2020-12-29 20:33:57 +0100

    Move into CS229

--- linear_regression/src/main.rs
+++ linear_regression/src/main.rs
@@ -1,12 +1,14 @@
 use ndarray::prelude::*;
 use ndarray_linalg::solve::Inverse;
+use tensorflow::train::GradientDescentOptimizer;
 
 fn main() {
     let x = array![[1., 2.], [1., 5.], [1., 7.], [1., 8.]];
     let y = array![1., 2., 3., 3.];
     let w = array![0., 0.];
     // let h = h(w, x.row(0).to_owned());
-    let res = batch_gd(w, x, y, 0.01);
+    // let res = batch_gd(w, x, y, 0.01);
+    let res = normal_eq(x, y);
     dbg!(res);
 }
 
@@ -39,12 +41,16 @@ fn cost(w: &Array1<f64>, x: &Array2<f64>, y: &Array1<f64>, j: usize) -> f64 {
     sum
 }
 
+// w: weights
+// x: values evaluated in prediction function
+// y: actual output values for x values in prediction function
+// a: learning rate, usually good rule of thumb to set at 0.01, 0.02, 0.04 and so on and
+// empirically determine which is most efficient
 fn batch_gd(mut w: Array1<f64>, x: Array2<f64>, y: Array1<f64>, a: f64) -> Array1<f64> {
     // Using indices to travers the weights is necessary as this prevents long lived borrows and
     // allows w to be mutated and passed to the cost function.
     for _n in 1..10000 {
         for j in 0..w.len() {
-            // Not sure if clones here can be avoided. Maybe passing by ref?
             let sum = cost(&w, &x, &y, j);
             w[j] -= a * sum;
         }
@@ -53,4 +59,20 @@ fn batch_gd(mut w: Array1<f64>, x: Array2<f64>, y: Array1<f64>, a: f64) -> Array
     w
 }
 
-fn stochastic_gd() {}
+// The sum is calculated only for one row of the data instead of for all rows before updating the
+// weights.
+fn stochastic_gd(mut w: Array1<f64>, x: Array2<f64>, y: Array1<f64>, a: f64) -> Array1<f64> {
+    for _n in 1..10000 {
+        for j in 0..w.len() {}
+    }
+
+    unimplemented!()
+}
+
+// Normal equation calculation.
+fn normal_eq(x: Array2<f64>, y: Array1<f64>) -> Array1<f64> {
+    let x_t = x.clone().reversed_axes();
+    let left = x_t.dot(&x).inv().unwrap();
+    let right = x_t.dot(&y);
+    left.dot(&right)
+}
